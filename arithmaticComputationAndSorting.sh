9ae85c0bbe62b7fe89669dcdfc297b57e57e7d0a
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-02-25 19:02:36 +0530

    MODIFIED:Compute the value of (c+a/b)

--- arithmaticComputationAndSorting.sh
+++ arithmaticComputationAndSorting.sh
@@ -6,4 +6,5 @@ read -p "Enter second number: " number2
 read -p "Enter third number: " number3
 
 result1=`echo "scale=2 ; $number1+$number2*$number3" | bc -l`
-result2=`echo "scale=2 ; $number3+$number1/$number2" | bc -l`  
+result2=`echo "scale=2 ; $number3+$number1/$number2" | bc -l`
+result3=`echo "scale=2 ; $number1*$number2+number3" | bc -l`
