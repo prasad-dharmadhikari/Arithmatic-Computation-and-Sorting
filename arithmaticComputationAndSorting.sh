f098f47ca82cca6817ed6aea919ebfcec4eedd5c
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-02-25 19:22:35 +0530

    MODIFIED: Stored every result in dictionary

--- arithmaticComputationAndSorting.sh
+++ arithmaticComputationAndSorting.sh
@@ -4,8 +4,13 @@
 read -p "Enter first number: " number1
 read -p "Enter second number: " number2
 read -p "Enter third number: " number3
-
+declare -A arithmaticDictionary
 result1=`echo "scale=2 ; $number1+$number2*$number3" | bc -l`
+arithmaticDictionary["first"]=$result1
 result2=`echo "scale=2 ; $number3+$number1/$number2" | bc -l`
+arithmaticDictionary["second"]=$result2
 result3=`echo "scale=2 ; $number1*$number2+$number3" | bc -l`
+arithmaticDictionary["third"]=$result3
 result4=`echo "scale=2 ; $number1%$number2+$number3" | bc -l`
+arithmaticDictionary["fourth"]=$result4
+
