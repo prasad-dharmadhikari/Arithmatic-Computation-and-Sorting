7738df2c79a97f9ef0a51fddac294576fb1f8e04
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-02-25 19:11:26 +0530

    MODIFIED:Corrected syntax

--- arithmaticComputationAndSorting.sh
+++ arithmaticComputationAndSorting.sh
@@ -7,5 +7,5 @@ read -p "Enter third number: " number3
 
 result1=`echo "scale=2 ; $number1+$number2*$number3" | bc -l`
 result2=`echo "scale=2 ; $number3+$number1/$number2" | bc -l`
-result3=`echo "scale=2 ; $number1*$number2+number3" | bc -l`
+result3=`echo "scale=2 ; $number1*$number2+$number3" | bc -l`
 result4=`echo "scale=2 ; $number1%$number2+$number3" | bc -l`
