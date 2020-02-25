6673b804131c4da05682f96415a5cffe5cd3100e
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-02-25 18:51:44 +0530

    MODIFIED:Compute the value of (a+b*c)

--- arithmaticComputationAndSorting.sh
+++ arithmaticComputationAndSorting.sh
@@ -5,3 +5,4 @@ read -p "Enter first number: " number1
 read -p "Enter second number: " number2
 read -p "Enter third number: " number3
 
+result1=`echo "scale=2 ; $number1+$number2*$number3" | bc -l`
