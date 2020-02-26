ce2d2d09b7e75e066a09935308146e4637a08366
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-02-26 12:11:17 +0530

    MODIFIED: Added spaces between comment and code

--- arithmaticComputationAndSorting.sh
+++ arithmaticComputationAndSorting.sh
@@ -1,14 +1,17 @@
 #!/bin/bash -x
 
 # Variables
+
 temp=0
 declare -A arithmaticDictionary
 declare -a arithmaticArray
 # Take input from user
+
 read -p "Enter first number: " number1
 read -p "Enter second number: " number2
 read -p "Enter third number: " number3
 #Compute the result and store in dictionary
+
 result1=`echo "scale=2 ; $number1+$number2*$number3" | bc -l`
 arithmaticDictionary[1]=$result1
 result2=`echo "scale=2 ; $number3+$number1/$number2" | bc -l`
@@ -18,11 +21,13 @@ arithmaticDictionary[3]=$result3
 result4=`echo "scale=2 ; $number1%$number2+$number3" | bc -l`
 arithmaticDictionary[4]=$result4
 #Store the elements of dictionary in array
+
 for value in ${!arithmaticDictionary[@]}
 do
 	arithmaticArray[$value]="${arithmaticDictionary[$value]}"
 done
 # Sort the elements in array in descending order
+
 for count1 in ${!arithmaticArray[@]}
 do
 	for count2 in ${!arithmaticArray[@]}
@@ -37,6 +42,7 @@ do
 done
 echo "Sorted array in descending order is:" ${arithmaticArray[@]}
 # Sort the elements in array in ascending order
+
 for count1 in ${!arithmaticArray[@]}
 do
 	for count2 in ${!arithmaticArray[@]}
